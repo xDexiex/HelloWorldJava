f8ab5db6c5d3e54603994dc0614ec2ecff3dadc4
u1456914 <u1456914@unimail.hud.ac.uk>
2019-01-15 11:40:10 +0000

    Basic Hello World

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by u1456914 on 15/01/2019.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
